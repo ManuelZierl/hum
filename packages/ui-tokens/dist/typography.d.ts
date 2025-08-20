9ae9d45446a61100efb3b6d093680a7ae99abaf3
ManuelZierl <manuel.zierl@web.de>
2025-08-20 08:41:55 +0200

    feat: add ui tokens package (#2)

---
+++ packages/ui-tokens/dist/typography.d.ts
@@ -0,0 +1,16 @@
+export declare const typography: {
+    fontFamily: string;
+    fontSize: {
+        xs: number;
+        sm: number;
+        md: number;
+        lg: number;
+        xl: number;
+    };
+    fontWeight: {
+        regular: string;
+        medium: string;
+        bold: string;
+    };
+};
+export type Typography = typeof typography;
