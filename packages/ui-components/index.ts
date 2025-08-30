1218b09fcf32cb8b061d7e6eb1defa09173d2585
ManuelZierl <manuel.zierl@web.de>
2025-08-30 12:26:05 +0200

    feat: add React Native badge component

--- packages/ui-components/index.ts
+++ packages/ui-components/index.ts
@@ -9,3 +9,5 @@ export {
 export type { AccordionProps } from './src/accordion';
 export { Alert, AlertTitle, AlertDescription } from './src/alert';
 export type { AlertProps, AlertVariant } from './src/alert';
+export { Badge } from './src/badge';
+export type { BadgeProps, BadgeVariant } from './src/badge';
