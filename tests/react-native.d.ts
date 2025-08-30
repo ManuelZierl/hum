f7556cf010f5d1fa3605337df9d229a755d633aa
ManuelZierl <manuel.zierl@web.de>
2025-08-30 10:14:52 +0200

    fix: enable web testing and lint (#89)

--- tests/react-native.d.ts
+++ tests/react-native.d.ts
@@ -1,7 +1,19 @@
+/* eslint-disable @typescript-eslint/no-explicit-any, @typescript-eslint/no-unused-vars, @typescript-eslint/no-empty-object-type */
 declare module 'react-native' {
   import * as React from 'react';
   export const View: React.FC<any>;
   export const Text: React.FC<any>;
   export const Image: React.FC<any>;
+  export const Pressable: React.FC<any>;
+  export const Animated: any;
   export const StyleSheet: { create: (styles: any) => any };
+  export type StyleProp<T> = any;
+  export interface ViewStyle {}
+  export interface TextStyle {}
+  export interface ViewProps {}
+  export interface PressableProps {
+    disabled?: boolean;
+    [key: string]: any;
+  }
+  export function useColorScheme(): 'light' | 'dark';
 }
