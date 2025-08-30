c501d0b00f8c5c7251592e412e06c9ba8d140b28
ManuelZierl <manuel.zierl@web.de>
2025-08-30 11:14:04 +0200

    feat: add AlertDialog component and stories

--- packages/ui-components/index.ts
+++ packages/ui-components/index.ts
@@ -7,3 +7,18 @@ export {
   AccordionContent,
 } from './src/accordion';
 export type { AccordionProps } from './src/accordion';
+
+export {
+  AlertDialog,
+  AlertDialogPortal,
+  AlertDialogOverlay,
+  AlertDialogTrigger,
+  AlertDialogContent,
+  AlertDialogHeader,
+  AlertDialogFooter,
+  AlertDialogTitle,
+  AlertDialogDescription,
+  AlertDialogAction,
+  AlertDialogCancel,
+} from './src/alert-dialog';
+export type { AlertDialogProps } from './src/alert-dialog';
