4ae5ea664c9008ac002db3eba175cd0565f8c050
ManuelZierl <manuel.zierl@web.de>
2025-08-30 12:35:49 +0200

    feat: add React Native button component

--- packages/ui-components/index.ts
+++ packages/ui-components/index.ts
@@ -9,3 +9,5 @@ export {
 export type { AccordionProps } from './src/accordion';
 export { Alert, AlertTitle, AlertDescription } from './src/alert';
 export type { AlertProps, AlertVariant } from './src/alert';
+export { Button } from './src/button';
+export type { ButtonProps, ButtonVariant, ButtonSize } from './src/button';
