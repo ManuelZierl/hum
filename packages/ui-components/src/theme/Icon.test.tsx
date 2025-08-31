849ab0306851b0d180328e0277ffa881c17bda7a
ManuelZierl <manuel.zierl@web.de>
2025-08-31 20:41:18 +0200

    test: relax Icon DOM typings

--- packages/ui-components/src/theme/Icon.test.tsx
+++ packages/ui-components/src/theme/Icon.test.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import React from 'react';
 import { render } from '@testing-library/react';
 import '@testing-library/jest-dom';
@@ -5,7 +6,6 @@ import { Icon, type IconProps } from './Icon';
 import { ThemeProvider } from './ThemeProvider';
 import { colors } from './colors';
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 const expectAny = expect as any;
 
 describe('Icon', () => {
@@ -19,20 +19,20 @@ describe('Icon', () => {
 
   it('uses theme foreground color by default', () => {
     const { container } = renderIcon();
-    const svg = (container as unknown as HTMLElement).querySelector('svg');
+    const svg = (container as any).querySelector('svg');
     expectAny(svg).toHaveAttribute('fill', colors.light.foreground);
   });
 
   it('allows overriding color', () => {
     const custom = '#123456';
     const { container } = renderIcon({ color: custom });
-    const svg = (container as unknown as HTMLElement).querySelector('svg');
+    const svg = (container as any).querySelector('svg');
     expectAny(svg).toHaveAttribute('fill', custom);
   });
 
   it('applies size to width and height', () => {
     const { container } = renderIcon({ size: 32 });
-    const svg = (container as unknown as HTMLElement).querySelector('svg');
+    const svg = (container as any).querySelector('svg');
     expectAny(svg).toHaveAttribute('width', '32');
     expectAny(svg).toHaveAttribute('height', '32');
   });
