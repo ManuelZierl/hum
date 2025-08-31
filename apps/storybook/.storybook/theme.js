a092125f535c0d47fab193f993c6437868f1a2e6
Manuel Zierl <manuel.zierl@web.de>
2025-08-31 17:42:59 +0200

    add hum logo to storybook

---
+++ apps/storybook/.storybook/theme.js
@@ -0,0 +1,9 @@
+import { create } from 'storybook/theming';
+import logo from '../../../imgs/logo-transparent.svg';
+ 
+export default create({
+  base: 'light',
+  brandTitle: 'Hum Storybook',
+  brandImage: logo,
+  brandTarget: '_self',
+});
