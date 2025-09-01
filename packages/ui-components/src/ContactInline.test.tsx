7a6226758a4391128be55d4713c16ae95041836a
Manuel Zierl <manuel.zierl@web.de>
2025-09-01 19:45:09 +0200

    remoev unused components

--- packages/ui-components/src/ContactInline.test.tsx
+++ packages/ui-components/src/ContactInline.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react-native';
 import '@testing-library/jest-native/extend-expect';
 import { ThemeProvider } from './theme/ThemeProvider';
-import { ContactInline } from './people/ContactInline';
+import { ContactInline } from './ContactInline';
 
 describe('ContactInline', () => {
   function renderCI(online?: boolean) {
