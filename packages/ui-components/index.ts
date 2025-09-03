320d9d542f4e6d47ebb535b2a657e754e1a3a200
ManuelZierl <manuel.zierl@web.de>
2025-09-03 19:42:34 +0200

    remoev unused components (#169)

--- packages/ui-components/index.ts
+++ packages/ui-components/index.ts
@@ -1,39 +1,12 @@
 export { ThemeProvider, useTheme } from './src/theme/ThemeProvider';
-export {
-  Accordion,
-  AccordionItem,
-  AccordionTrigger,
-  AccordionContent,
-} from './src/accordion';
-export type { AccordionProps } from './src/accordion';
-export {
-  AlertDialog,
-  AlertDialogPortal,
-  AlertDialogOverlay,
-  AlertDialogTrigger,
-  AlertDialogContent,
-  AlertDialogHeader,
-  AlertDialogFooter,
-  AlertDialogTitle,
-  AlertDialogDescription,
-  AlertDialogAction,
-  AlertDialogCancel,
-} from './src/alert-dialog';
-export type { AlertDialogProps } from './src/alert-dialog';
-export { Alert, AlertTitle, AlertDescription } from './src/alert';
-export type { AlertProps, AlertVariant } from './src/alert';
 export { Avatar, AvatarImage, AvatarFallback } from './src/avatar';
 export type {
   AvatarProps,
   AvatarImageProps,
   AvatarFallbackProps,
 } from './src/avatar';
-export { Button } from './src/button';
-export type { ButtonProps, ButtonVariant, ButtonSize } from './src/button';
-export { Badge } from './src/badge';
-export type { BadgeProps, BadgeVariant } from './src/badge';
-export { AspectRatio } from './src/aspect-ratio';
-export type { AspectRatioProps } from './src/aspect-ratio';
+export { buttonVariants, Button } from './src/button';
+export type { ButtonVariant, ButtonSize } from './src/button';
 export { MessageBubble } from './src/message-bubble';
 export type { MessageBubbleProps } from './src/message-bubble';
 export { BottomNavItem } from './src/bottom-navigation-item';
@@ -50,8 +23,7 @@ export { ChatItem } from './src/ChatItem';
 export type { ChatItemProps } from './src/ChatItem';
 export { TopBar } from './src/TopBar';
 export type { TopBarProps } from './src/TopBar';
-export { ContactInline } from './src/people/ContactInline';
-export type { ContactInlineProps } from './src/people/ContactInline';
-
 export { Icon } from './src/theme/Icon';
 export type { IconName, IconProps } from './src/theme/Icon';
+export { ContactInline } from './src/ContactInline';
+export type { ContactInlineProps } from './src/ContactInline';
