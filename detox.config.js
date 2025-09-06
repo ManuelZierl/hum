21bae245c65ed606de52f50ccf1d85e53d040fca
Manuel Zierl <manuel.zierl@web.de>
2025-09-06 15:31:57 +0200

    fix detox

--- detox.config.js
+++ detox.config.js
@@ -3,28 +3,40 @@
 module.exports = {
   testRunner: 'jest',
   runnerConfig: 'detox/jest.config.js',
-  configurations: {
-    'ios.sim.debug': {
+  apps: {
+    'myApp.ios': {
+      type: 'ios.app',
+      binaryPath:
+        'apps/mobile/ios/build/Build/Products/Debug-iphonesimulator/hum.app',
+      build:
+        'xcodebuild -workspace apps/mobile/ios/hum.xcworkspace -scheme hum -configuration Debug -sdk iphonesimulator -derivedDataPath apps/mobile/ios/build',
+    },
+    'myApp.android': {
+      type: 'android.apk',
+      binaryPath:
+        'apps/mobile/android/app/build/outputs/apk/debug/app-debug.apk',
+      build:
+        'cd apps/mobile/android && ./gradlew assembleDebug assembleAndroidTest -DtestBuildType=debug',
+    },
+  },
+  devices: {
+    'ios.sim': {
       type: 'ios.simulator',
       device: { type: 'iPhone 15' },
-      app: {
-        type: 'ios.app',
-        binaryPath:
-          'apps/mobile/ios/build/Build/Products/Debug-iphonesimulator/hum.app',
-        build:
-          'xcodebuild -workspace apps/mobile/ios/hum.xcworkspace -scheme hum -configuration Debug -sdk iphonesimulator -derivedDataPath apps/mobile/ios/build',
-      },
     },
-    'android.emu.debug': {
+    emulator: {
       type: 'android.emulator',
       device: { avdName: 'test' },
-      app: {
-        type: 'android.apk',
-        binaryPath:
-          'apps/mobile/android/app/build/outputs/apk/debug/app-debug.apk',
-        build:
-          'cd apps/mobile/android && ./gradlew assembleDebug assembleAndroidTest -DtestBuildType=debug',
-      },
+    },
+  },
+  configurations: {
+    'ios.sim.debug': {
+      device: 'ios.sim',
+      app: 'myApp.ios',
+    },
+    'android.emu.debug': {
+      device: 'emulator',
+      app: 'myApp.android',
     },
   },
 };
