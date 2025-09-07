82f8ef4143ff5f84278b4f3b53932ee0599680d7
Manuel Zierl <manuel.zierl@web.de>
2025-09-07 09:44:37 +0200

    fix rootDir

--- detox/jest.config.js
+++ detox/jest.config.js
@@ -4,7 +4,7 @@ module.exports = {
   rootDir: '.',
   testEnvironment: 'node',
   testTimeout: 240000,
-  setupFilesAfterEnv: ['<rootDir>/detox/setup.js'],
+  setupFilesAfterEnv: ['<rootDir>/setup.js'],
   // Only pick up Detox E2E specs under the detox/ folder
-  testMatch: ['<rootDir>/detox/**/*.spec.js'],
+  testMatch: ['<rootDir>/**/*.spec.js'],
 };
