f0424c6e80de437298a7662354cf28cf4198be31
Manuel Zierl <manuel.zierl@web.de>
2025-09-07 15:56:07 +0200

    fix detox

--- apps/mobile/android/app/src/androidTest/java/com/hum/app/DetoxTest.java
+++ apps/mobile/android/app/src/androidTest/java/com/hum/app/DetoxTest.java
@@ -23,9 +23,9 @@ public class DetoxTest {
         DetoxConfig detoxConfig = new DetoxConfig();
         detoxConfig.idlePolicyConfig.masterTimeoutSec = 90;
         detoxConfig.idlePolicyConfig.idleResourceTimeoutSec = 60;
-        detoxConfig.rnContextLoadTimeoutSec = (BuildConfig.DEBUG ? 180 : 60);
+        // Give RN more time to load in debug (CI emulators can be slow)
+        detoxConfig.rnContextLoadTimeoutSec = (BuildConfig.DEBUG ? 300 : 60);
 
         Detox.runTests(mActivityRule, detoxConfig);
     }
 }
-
--- detox/jest.config.js
+++ detox/jest.config.js
@@ -4,7 +4,8 @@ module.exports = {
   rootDir: '.',
   testEnvironment: 'detox/runners/jest/testEnvironment',
   testRunner: 'jest-circus/runner',
-  testTimeout: 240000,
+  // Allow more time on CI where emulators and builds are slower
+  testTimeout: process.env.CI ? 600000 : 240000,
   // Optional: keep a lightweight setup file for test tweaks.
   // If you don't need it, you can remove this line.
   setupFilesAfterEnv: ['<rootDir>/setup.js'],
