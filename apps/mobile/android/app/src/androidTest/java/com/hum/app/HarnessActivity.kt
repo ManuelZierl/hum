fb6ae7726dfe99c0b7116dc26f1e6fb405d39976
ManuelZierl <manuel.zierl@web.de>
2025-09-08 21:12:32 +0200

    chore: switch to espresso tests

---
+++ apps/mobile/android/app/src/androidTest/java/com/hum/app/HarnessActivity.kt
@@ -0,0 +1,7 @@
+package com.hum.app
+
+import com.facebook.react.ReactActivity
+
+class HarnessActivity : ReactActivity() {
+  override fun getMainComponentName(): String = "main"
+}
--- detox/setup.js
+++
@@ -1,4 +0,0 @@
-// Keep this file for lightweight Jest tweaks only.
-// Detox init/cleanup is handled by the Detox Jest environment.
-// Allow ample time for emulator boot and app launch in CI.
-jest.setTimeout(600000);
