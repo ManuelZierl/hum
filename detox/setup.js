2b719c2c070a80aae61816020d0cb6ff0a05ea08
ManuelZierl <manuel.zierl@web.de>
2025-09-08 18:29:58 +0200

    chore: stabilize detox ci

--- detox/setup.js
+++ detox/setup.js
@@ -1,3 +1,4 @@
 // Keep this file for lightweight Jest tweaks only.
 // Detox init/cleanup is handled by the Detox Jest environment.
-jest.setTimeout(240000);
+// Allow ample time for emulator boot and app launch in CI.
+jest.setTimeout(600000);
